355ffa0b44ab0617c9cb25a8eca7734903c730fd
otisnth <otisnth@gmail.com>
2024-05-02 17:26:06 +0300

    feat: локализация primevue

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,6 +9,7 @@ import { createInertiaApp } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
 import PrimeVue from "primevue/config";
+import locale from "@/primevue.config.js";
 
 const appName =
     window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
@@ -24,7 +25,9 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
-            .use(PrimeVue)
+            .use(PrimeVue, {
+                locale: locale,
+            })
             .mount(el);
     },
     progress: {
