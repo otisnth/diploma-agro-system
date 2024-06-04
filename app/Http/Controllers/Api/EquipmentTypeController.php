2824f332e575a7421c1048e10b87c7d341f4d4cf
otisnth <otisnth@gmail.com>
2024-06-04 18:57:02 +0300

    feat: удалена иконка для типа оборудования

--- app/Http/Controllers/Api/EquipmentTypeController.php
+++ app/Http/Controllers/Api/EquipmentTypeController.php
@@ -20,73 +20,6 @@ class EquipmentTypeController extends Controller
 
     protected $policy = TruePolicy::class;
 
-    protected function afterIndex(Request $request, $entities)
-    {
-        $newCollection = $entities->map(function ($item, $key) {
-            $item->icon = $item->icon ? asset(Storage::url($item->icon)) : null;
-            return $item;
-        });
-        
-        return $newCollection;
-    }
-
-    protected function afterShow(Request $request, Model $entity)
-    {
-        $entity->icon = $entity->icon ? asset(Storage::url($entity->icon)) : null;
-    }
-
-    protected function afterDestroy(Request $request, Model $entity)
-    {   
-        if($entity->getAttribute("icon"))
-        {
-            unlink($_SERVER['DOCUMENT_ROOT'].Storage::url($entity->getAttribute("icon")));
-        }
-    }
-
-    protected function performStore(Request $request, Model $entity, array $attributes): void
-    {
-
-        $base64_str = substr($request->icon, strpos($request->icon, ",")+1);
-        $icon = base64_decode($base64_str);
-        
-        $fileName = time().'.'.'png';
-        $success = file_put_contents(public_path().'\\storage\\icons\\'.$fileName, $icon);
-
-        if($success) {
-            $attributes['icon'] = 'public/icons/'.$fileName;
-        }
-        else {
-            $attributes['icon'] = null;
-        }
-
-        $this->performFill($request, $entity, $attributes);
-        $entity->save();
-    }
-
-    protected function performUpdate(Request $request, Model $entity, array $attributes): void
-    {
-
-        if (!filter_var($request->icon, FILTER_VALIDATE_URL)) {
-
-            $base64_str = substr($request->icon, strpos($request->icon, ",")+1);
-            $icon = base64_decode($base64_str);
-            
-            $fileName = time().'.'.'png';
-            $success = file_put_contents(public_path().'\\storage\\icons\\'.$fileName, $icon);
-
-            if ($entity->icon && $success) {
-                unlink($_SERVER['DOCUMENT_ROOT'].Storage::url($entity->icon));
-            }
-
-            $attributes['icon'] = 'public/icons/'.$fileName;
-        }
-        else {
-            $attributes['icon'] = $entity->icon;
-        }
-
-        $this->performFill($request, $entity, $attributes);
-        $entity->save();
-    }
 
     public function properties()
     {
@@ -98,13 +31,6 @@ class EquipmentTypeController extends Controller
                     'type' => 'text',
                     'required' => 'true',
                     'sortable'=> true
-                ], 
-                [
-                    'title' => 'Иконка',
-                    'key' => 'icon',
-                    'type' => 'image',
-                    'required' => 'true',
-                    'sortable'=> false
                 ],
             ],
             'filters' => [
